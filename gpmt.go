d5e4663d22b54d2bc540296de4c3520b42dcf3e9
Faisal Ali <fai28683@gmail.com>
2018-08-14 17:56:43 +0100

    Cleanup and restructuring of code
    + cleaned up lots of white space.
    + moved the code around based in the order of execution for easy reading
    + added a help output when there is no command on the supplied on the gpmt tool

--- gpmt.go
+++ gpmt.go
@@ -1,9 +1,7 @@
-/*
-Greenplum magic tool
+/* Greenplum magic tool
 
 Authored by Tyler Ramer, Ignacio Elizaga
 Copyright 2018
-
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -17,19 +15,17 @@ Copyright 2018
    See the License for the specific language governing permissions and
    limitations under the License.
 
-
 */
 
 package main
 
 import (
 	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/pivotal-gss/gpmt2/cmd"
 	"io"
 	"os"
 	"time"
-
-	log "github.com/Sirupsen/logrus"
-	"github.com/pivotal-gss/gpmt2/cmd"
 )
 
 // TODO: maybe clean this up by adding a logrus file hook
